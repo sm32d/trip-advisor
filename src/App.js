2a9867b3d3015ff2634232ea777bbed3db3a3d12
Sulabh Mehta (Dominik) <64636803+sm32d@users.noreply.github.com>
2022-07-25 17:25:03 +0800

    Added browser navigator user location

--- src/App.js
+++ src/App.js
@@ -9,11 +9,23 @@ import Map from "./components/Map/Map"
 const App = () => {
 
     const [places, setPlaces] = useState([])
+    const [coordinates, setCoordinates] = useState({lat: 0, lng: 0})
+    const [bounds, setBounds] = useState(null)
 
     useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            setCoordinates({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            })
+        })
+    }, [])
+
+    useEffect(() => {
+        console.log(coordinates, bounds)
         getPlaces()
             .then(data => setPlaces(data))
-    }, [])
+    }, [coordinates, bounds])
 
     return (
         <>
@@ -26,7 +38,7 @@ const App = () => {
                     <List />
                 </Grid>
                 <Grid item xs={12} md={8}>
-                    <Map />
+                    <Map setCoordinates={setCoordinates} setBounds={setBounds} coordinates={coordinates} />
                 </Grid>
             </Grid>
         </>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -6,19 +6,22 @@ import { Rating } from "@material-ui/lab";
 
 import useStyles from './styles'
 
-const Map = () => {
+const Map = ({setCoordinates, setBounds, coordinates}) => {
     const classes = useStyles();
     const isMobile = useMediaQuery('(max-width:600px)');
     return (
         <div className={classes.mapContainer}>
             <GoogleMapReact
                 bootstrapURLKeys={{key: 'AIzaSyD-9tSrke72PouQMnMX-a7eZk-c2zrq6i0'}} // use your own API key
-                defaultCenter={{lat: -34.397, lng: 150.644}}
-                center={{lat: -34.397, lng: 150.644}}
+                defaultCenter={coordinates}
+                center={coordinates}
                 defaultZoom={14}
                 margin={[50, 50, 50, 50]}
                 options={''}
-                onChange={''}
+                onChange={(e) => {
+                    setCoordinates({lat: e.center.lat, lng: e.center.lng});
+                    setBounds({ne: e.marginBounds.ne, sw: e.marginBounds.sw});
+                }}
                 onChildClick={''}
             >
 
